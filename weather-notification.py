29f3e9dfb2a0856101b1764e52d656ea7a33a410
Ali DN <alidelaramnikoo@gmail.com>
2026-03-13 22:15:42 +0100

    implement initial weather notifier using Open-Meteo API

--- weather-notification.py
+++ weather-notification.py
@@ -3,11 +3,11 @@ from plyer import notification
 
 
 try:
-    city = input("please enter a valid city:")
+    city = input("please enter a valid city:").lower()
     geo_url = "https://geocoding-api.open-meteo.com/v1/search"
     geo_param = {"name" : city }
     geo_res = requests.get(geo_url, params=geo_param ).json()
-    print(geo_res)
+    #print(geo_res)
 
 
     if "results" in geo_res:
@@ -17,19 +17,22 @@ try:
         weather_url = "https://api.open-meteo.com/v1/forecast"
         weather_param = {"latitude" : lat, "longitude" : lon, "current_weather" : True}
         weather_res = requests.get(weather_url, params= weather_param).json()
-        print(weather_res)
+        #print(weather_res)
 
         if "current_weather" in weather_res:
             temp = weather_res["current_weather"]["temperature"]
             wind = weather_res["current_weather"]["windspeed"]
 
             weather_info = f"weather in {city}: temperature is {temp}°C and wind is {wind} km/h"
-            print(weather_info)
+            #print(weather_info)
 
-            notification.notify(title = "weather update", message = weather_info )
+            notification.notify(title = "weather update", message = weather_info,timeout = 6 )
 
         else:
             print("weather data not found")
+    else:
+        print(f"the {city} is not found. please check the spelling.")
+        notification.notify(title='Wrong City', message = f'{city} not Found', timeout= 5)
 
 except(requests.exceptions.ConnectionError):
     print("check your internet connection")
